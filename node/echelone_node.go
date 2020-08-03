aa942972b98a30a3f30c5d050debc8799c6f2de6
fkorotkov <fedor.korotkov@gmail.com>
2020-08-03 16:48:05 -0400

    Fixed formatting

--- node/echelone_node.go
+++ node/echelone_node.go
@@ -153,16 +153,16 @@ func formatDuration(duration time.Duration) string {
 		return fmt.Sprintf("%.1fs", float64(duration.Milliseconds())/1000)
 	}
 	if duration < time.Minute {
-		return fmt.Sprintf("%ds", int(math.Floor(duration.Seconds())))
+		return fmt.Sprintf("%ds", int(math.Floor(duration.Seconds()))%60)
 	}
 	if duration < time.Hour {
-		return fmt.Sprintf("%02d:%02d", int(math.Floor(duration.Minutes())), int(math.Floor(duration.Seconds())))
+		return fmt.Sprintf("%02d:%02d", int(math.Floor(duration.Minutes()))%60, int(math.Floor(duration.Seconds()))%60)
 	}
 	return fmt.Sprintf(
 		"%02d:%02d:%02d",
 		int(math.Floor(duration.Hours())),
-		int(math.Floor(duration.Minutes())),
-		int(math.Floor(duration.Seconds())),
+		int(math.Floor(duration.Minutes()))%60,
+		int(math.Floor(duration.Seconds()))%60,
 	)
 }
 
