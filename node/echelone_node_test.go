8f47165cb32b7351dc9798f1cb1ee480c9591172
fkorotkov <fedor.korotkov@gmail.com>
2020-08-05 17:02:57 -0400

    Fixed tests

--- node/echelone_node_test.go
+++ node/echelone_node_test.go
@@ -7,12 +7,13 @@ import (
 )
 
 func Test_AppendDescription(t *testing.T) {
-	node := StartNewEchelonNode("test")
+	config := NewDefaultRenderingConfig()
+	node := StartNewEchelonNode("test", config)
 	node.AppendDescription("1")
 	node.AppendDescription("2")
 	node.AppendDescription("3")
 	node.AppendDescription("4")
 	node.AppendDescription("5")
 	node.AppendDescription("6")
-	assert.Equal(t, "23456", strings.Join(node.description, ""))
+	assert.Equal(t, "123456", strings.Join(node.description, ""))
 }
