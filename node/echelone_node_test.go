abb5ec5457fb9f198bd1cad2de04db21e0c91c49
fkorotkov <fedor.korotkov@gmail.com>
2020-08-06 16:32:51 -0400

    Fixed test compilation

--- node/echelone_node_test.go
+++ node/echelone_node_test.go
@@ -9,11 +9,11 @@ import (
 func Test_AppendDescription(t *testing.T) {
 	config := NewDefaultRenderingConfig()
 	node := StartNewEchelonNode("test", config)
-	node.AppendDescription("1")
-	node.AppendDescription("2")
-	node.AppendDescription("3")
-	node.AppendDescription("4")
-	node.AppendDescription("5")
-	node.AppendDescription("6")
+	node.Infof("1")
+	node.Infof("2")
+	node.Infof("3")
+	node.Infof("4")
+	node.Infof("5")
+	node.Infof("6")
 	assert.Equal(t, "123456", strings.Join(node.description, ""))
 }
