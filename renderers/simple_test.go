2f077ae796be4aa95e041593b6c65451c2b2dd44
Fedor Korotkov <fedor.korotkov@gmail.com>
2020-09-11 14:50:44 -0400

    Detect Quotes in Scopes (#11)
    * Detect Quotes in Scopes
    
    * Fixed unrelated linting issue
    
    * Added tests

---
+++ renderers/simple_test.go
@@ -0,0 +1,19 @@
+//nolint:testpackage
+package renderers
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_quotedIfNeeded(t *testing.T) {
+	assert.Equal(t, "'foo'", quotedIfNeeded("foo"))
+
+	assert.Equal(t, "task 'foo'", quotedIfNeeded("task 'foo'"))
+	assert.Equal(t, "'foo' task", quotedIfNeeded("'foo' task"))
+	assert.Equal(t, "task 'foo' has finished", quotedIfNeeded("task 'foo' has finished"))
+
+	assert.Equal(t, "task \"foo\"", quotedIfNeeded("task \"foo\""))
+	assert.Equal(t, "\"foo\" task", quotedIfNeeded("\"foo\" task"))
+	assert.Equal(t, "task \"foo\" has finished", quotedIfNeeded("task \"foo\" has finished"))
+}
